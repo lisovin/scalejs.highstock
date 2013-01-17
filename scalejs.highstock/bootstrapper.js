2c6dd11850ce64f7685c5a2ebbc0c64f07cf7578
Peter Lisovin <peter@lisovin.com>
2013-01-17 10:52:54 -0500

    Initial revision.

---
+++ scalejs.highstock/bootstrapper.js
@@ -0,0 +1,9 @@
+/*global require*/
+require({
+    "paths":  {
+        "scalejs":  "Scripts/scalejs-0.1.12",
+        "knockout" : "Scripts/knockout-2.2.1.debug",
+		"knockout.mapping" : "Scripts/knockout.mapping-latest.debug",
+		"highstock" : "Scripts/highstock.src"
+    }
+}, ['scalejs.highstock']);
---
+++ scalejs.highstock/tools/install.ps1
@@ -0,0 +1,21 @@
+﻿param($installPath, $toolsPath, $package, $project)
+
+$project | 
+	Add-Paths "{
+		'jQuery'			: 'Scripts/jQuery-1.9.0',
+		'scalejs.highstock' : 'Scripts/scalejs.highstock-$($package.Version)',
+		'highstock'			: 'Scripts/highstock.src',
+		'knockout'			: 'Scripts/knockout-2.2.1',
+		'knockout.mapping'	: 'Scripts/knockout.mapping-latest'
+	}" |
+	Add-Shims "{
+		'jQuery'			: {
+			exports : 'jQuery'
+		},
+		'highstock'			: {
+			deps	: ['jQuery'],
+			exports : 'Highcharts'
+		}
+	}" |
+	Add-ScalejsExtension 'scalejs.highstock'
+    
