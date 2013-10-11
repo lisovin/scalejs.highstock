d3e9cd464b8e8d2e783f06cb30e00a9e5a00a193
Peter Lisovin <peter@lisovin.com>
2013-10-11 17:01:51 -0400

    update to latest version of scalejs

--- scalejs.highstock/tools/install.ps1
+++ scalejs.highstock/tools/install.ps1
@@ -2,16 +2,10 @@
 
 $project | 
 	Add-Paths "{
-		'jQuery'			: 'Scripts/jQuery-1.9.0',
 		'scalejs.highstock' : 'Scripts/scalejs.highstock-$($package.Version)',
-		'highstock'			: 'Scripts/highstock.src',
-		'knockout'			: 'Scripts/knockout-2.2.1',
-		'knockout.mapping'	: 'Scripts/knockout.mapping-latest'
+		'highstock'			: 'Scripts/highstock.src'
 	}" |
 	Add-Shims "{
-		'jQuery'			: {
-			exports : 'jQuery'
-		},
 		'highstock'			: {
 			deps	: ['jQuery'],
 			exports : 'Highcharts'
